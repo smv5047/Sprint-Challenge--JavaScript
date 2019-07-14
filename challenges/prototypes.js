6730b68d7bfb9295c57da6ab9b6283b7ac417a65
smv5047 <smv5047@gmail.com>
2019-07-14 19:26:39 -0400

    classes challenge completed

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -30,7 +30,7 @@ CuboidMaker.prototype.volume = function(attributes) {
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
 
-CuboidMaker.prototype.surfaceArea = function(length, width, height) {
+CuboidMaker.prototype.surfaceArea = function(attributes) {
   return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
 };
 
